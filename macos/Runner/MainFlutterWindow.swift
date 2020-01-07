c5bed5ccc5ee7b2598f00784c7eaa08a09a9aff0
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2020-01-07 20:56:36 +0300

    set macos window color to red

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -8,6 +8,8 @@ class MainFlutterWindow: NSWindow {
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
+    self.backgroundColor = NSColor.red
+
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
